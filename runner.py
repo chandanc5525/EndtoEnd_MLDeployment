ea51d52f2824c0feaf40e64eade8a6009e0fac6a
Chandan Chaudhari <110313705+chandanc5525@users.noreply.github.com>
2024-07-29 21:47:33 +0530

    loguru and try & except statements updated in runner.py file

--- runner.py
+++ runner.py
@@ -1,11 +1,17 @@
 from model_service import ModelService
+from loguru import logger # loguru is for logging purposes
 
-
+logger.add('app_log',rotation= '1 MB')
+logger.info('Running ModelService')
 def main():
     ml_service = ModelService()
     ml_service.load_model('rf_v1')
+    logger.warning('Please Ensure Indepedent Variable Data for Prediction Model Service')
     pred = ml_service.predict([85, 2015, 2, 20, 1, 1, 0, 0, 1])
     print(f'The predicted value is ',pred)
 
-if __name__ == '__main__':
-    main()
+try:
+    if __name__ == '__main__':
+        main()
+except:
+    logger.error('Error, Please Debug Your Code')
